5195b4cc4a5c58c97eb62765711f58d325dee28e
Sagar Dobariya <infodsagar@gmail.com>
2022-07-23 16:49:15 +1000

    07/23
    Mobile navbar close btn bug fixed

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -8,7 +8,8 @@ export const Navbar = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
   const [open, setOpen] = useState(false);
-  const ref = useRef();
+  const ref1 = useRef();
+  const ref2 = useRef();
 
   const handleScroll = debounce(() => {
     const currentScrollPos = window.pageYOffset;
@@ -30,16 +31,20 @@ export const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos, visible, handleScroll]);
 
-  const handleClick = () => {
+  const handleClick = (e) => {
     setOpen(!open);
+    console.log('ref2');
   };
 
   useEffect(() => {
     const checkIfClickedOutside = (e) => {
       // If the menu is open and the clicked target is not within the menu,
       // then close the menu
-      if (open && ref.current && !ref.current.contains(e.target)) {
-        setOpen(false);
+      if (!ref2.current.contains(e.target)) {
+        if (open && ref1.current && !ref1.current.contains(e.target)) {
+          console.log('ref1');
+          setOpen(false);
+        }
       }
     };
     document.addEventListener('mousedown', checkIfClickedOutside);
@@ -54,8 +59,8 @@ export const Navbar = () => {
       <nav
         className={
           visible
-            ? 'mb-6 flex  fixed right-10 top-0 z-10 duration-1000 transition-[top]'
-            : 'mb-6 flex  fixed right-10 top-[-70px] z-10 duration-1000 transition-[top]'
+            ? 'mb-6 flex  fixed right-10 top-0 z-10 duration-500 transition-[top]'
+            : 'mb-6 flex  fixed right-10 top-[-70px] z-10 duration-500 transition-[top]'
         }
       >
         <a href='/' className='md:ml-10 ml:5 mr-5 mb-[-5px]'>
@@ -64,8 +69,8 @@ export const Navbar = () => {
             alt='logo main'
             className={
               visible
-                ? 'md:h-20 h-16 p-2 md:left-8 left-6 fixed md:top-[-4px] top-[2px] duration-1000 transition-[top]'
-                : 'md:h-20 h-16 p-2 md:left-8 left-6 fixed md:top-[-70px] top-[-65px] duration-1000 transition-[top]'
+                ? 'md:h-20 h-16 p-2 md:left-8 left-6 fixed md:top-[-4px] top-[2px] duration-500 transition-[top]'
+                : 'md:h-20 h-16 p-2 md:left-8 left-6 fixed md:top-[-70px] top-[-65px] duration-500 transition-[top]'
             }
           />
         </a>
@@ -87,18 +92,20 @@ export const Navbar = () => {
         </a>
         <button
           type='button'
-          className='py-5 md:hidden text-xl cursor-pointer'
+          className='py-5 md:hidden text-xl cursor-pointer min-w-[50px]'
           onClick={handleClick}
+          ref={ref2}
+          id='btn-1'
         >
           <FontAwesomeIcon icon={open ? faClose : faBars}></FontAwesomeIcon>
         </button>
         <div
           className={
             open
-              ? 'flex flex-col items-center md:hidden top-[68px] min-w-[130px] right-0 fixed bg-[#040404ab]'
+              ? 'flex flex-col items-center md:hidden top-[68px] min-w-[130px] right-0 fixed bg-[#040404ab] '
               : 'hidden'
           }
-          ref={ref}
+          ref={ref1}
         >
           <a href='/' className='py-2' onClick={handleClick}>
             Home
@@ -120,14 +127,14 @@ export const Navbar = () => {
       <div
         className={
           visible
-            ? 'border-b-2 border-white fixed top-[65px] md:top-[75px] min-w-[100%] flex duration-1000 transition-[top]'
-            : 'border-b-2 border-white fixed top-[-60px] md:top-[-60px] min-w-[100%] flex duration-1000 transition-[top]'
+            ? 'border-b-2 border-white fixed top-[65px] md:top-[75px] min-w-[100%] flex duration-500 transition-[top]'
+            : 'border-b-2 border-white fixed top-[-60px] md:top-[-60px] min-w-[100%] flex duration-500 transition-[top]'
         }
       ></div>
       <div
         className={
           visible
-            ? ' md:h-[72px] h-[65px]  fixed top-[0px] md:top-[0px] bg-[#040404ab] min-w-[100%] flex duration-1000 transition-[top]'
+            ? ' md:h-[72px] h-[65px]  fixed top-[0px] md:top-[0px] bg-[#040404ab] min-w-[100%] flex duration-500 transition-[top]'
             : ' md:h-[72px] h-[65px] fixed top-[-65px] md:top-[-65px] min-w-[100%] flex duration-1000 transition-[top]'
         }
       ></div>

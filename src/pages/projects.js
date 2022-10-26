57ff965b0ec3a45831eb7d2ecbd9e5cf6105c60e
Sagar Dobariya <infodsagar@gmail.com>
2022-10-26 21:32:06 +1100

    1026

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,15 @@
 import linkred from '../images/linkred.svg';
 import linkora from '../images/linkora.svg';
 import linkblue from '../images/linkblue.svg';
+import linksea from '../images/linksea.svg';
 import githublogored from '../images/githublogored.svg';
 import githublogoora from '../images/githublogoora.svg';
 import githublogoblue from '../images/githublogoblue.svg';
-import servicess from '../images/servicess.PNG';
+import githubseagreen from '../images/githubseagreen.svg';
 import amazonss from '../images/amazonss.PNG';
 import regoss from '../images/regoss.PNG';
 import note from '../images/note.PNG';
+import chat from '../images/chat.PNG';
 
 export const Projects = () => {
   return (
@@ -18,6 +20,37 @@ export const Projects = () => {
           <div className='flex flex-col md:flex-row items-center my-24'>
             <div>
               <div className='text-lg py-2'>Featured Project</div>
+              <div className='text-xl py-2'>Live Chat</div>
+              <div
+                className='text-xl bg-[#0cc69e98] rounded-xl p-2 max-w-7xl'
+                style={{ fontFamily: 'Lucida Console, monospace' }}
+              >
+                Backend based on Socket.io, Node.js server hosted on Heroku.
+                Front-end created using React.js and Material UI deployed on
+                Netlify. Context provider and Reducer hooks used to pass data
+                effectively to components. Easy login without signup, Overview
+                of all online user, Join public or start private chat with any
+                online user Retain username and chat data by utilizing local
+                storage
+              </div>
+              <span className='text-xl'>
+                Node.js Socket.io React Material UI, Tailwind
+              </span>
+              <div className='flex align-center absolute md:relative'>
+                <a href='https://github.com/infodsagar/chat'>
+                  <img src={githubseagreen} alt='git logo' />
+                </a>
+                <a href='https://sagar-chat.netlify.app/'>
+                  <img src={linksea} alt='git logo' />
+                </a>
+              </div>
+            </div>
+            <div className='py-10'>
+              <img src={chat} alt='service' className='rounded-xl' />
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row items-center my-24'>
+            <div>
               <div className='text-xl py-2'>Online Notes Keeper</div>
               <div
                 className='text-xl bg-[#c60c3193] rounded-xl p-2 max-w-7xl'
